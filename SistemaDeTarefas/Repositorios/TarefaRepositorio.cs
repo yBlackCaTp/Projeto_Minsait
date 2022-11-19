d719f963d13a6ab46e7f541aadc43deec4e36706
Paulo Murilo <72311432+yBlackCaTp@users.noreply.github.com>
2022-11-19 00:18:20 -0300

    Add files via upload

--- SistemaDeTarefas/Repositorios/TarefaRepositorio.cs
+++ SistemaDeTarefas/Repositorios/TarefaRepositorio.cs
@@ -9,40 +9,62 @@ namespace SistemaDeTarefas.Repositorios
     {
         private readonly SistemaDeTarefasDBContext _dbContext;
 
-        public TarefaRepositorio (SistemaDeTarefasDBContext sistemaDeTarefasDBContext)
+        public TarefaRepositorio(SistemaDeTarefasDBContext sistemaDeTarefasDBContext)
         {
             _dbContext = sistemaDeTarefasDBContext;
         }
 
-        public Task<TarefaModel> Adicionar(TarefaModel tarefa)
+        public async Task<TarefaModel> Adicionar(TarefaModel tarefa)
         {
-            throw new NotImplementedException();
+            _dbContext.Tarefas.Update(tarefa);
+            await _dbContext.SaveChangesAsync();
+
+            return tarefa;
         }
 
-        public Task<bool> Apagar(int id)
+        public async Task<bool> Apagar(int id)
         {
-            throw new NotImplementedException();
+            TarefaModel tarefa = await BuscarPorId(id);
+
+            if (tarefa == null)
+            {
+                throw new Exception($"Id do Usuario: {tarefa} não localizado");
+
+            }
+
+            _dbContext.Tarefas.Remove(tarefa);
+            await _dbContext.SaveChangesAsync();
+
+            return true;
         }
 
-        public Task<TarefaModel> Atualizar(TarefaModel tarefa, int id)
+        public async Task<TarefaModel> Atualizar(TarefaModel tarefa, int id)
         {
-            throw new NotImplementedException();
+            TarefaModel tarefaModel  = await BuscarPorId(id);
+
+            if (tarefa == null)
+            {   
+                throw new Exception($"Id do tarefa: {tarefa} não localizado");
+
+            }
+
+            tarefa.Nome = tarefa.Nome;
+           
+
+            _dbContext.Tarefas.Update(tarefa);
+            await _dbContext.SaveChangesAsync();
+
+            return tarefa;
         }
 
-        public Task<TarefaModel> BuscarPorId(int id)
+        public async Task<TarefaModel> BuscarPorId(int id)
         {
-            throw new NotImplementedException();
+            return await _dbContext.Tarefas.Include(x => x.Id).FirstOrDefaultAsync(x => x.Id == id);
         }
-
-        //public Task<List<TarefaModel>> BuscarTarefas()
-        //{
-        //    throw new NotImplementedException();
-        //}
 
         public async Task<List<TarefaModel>> BuscarTodostarefas()
         {
-            return await _dbContext.Tarefas.Include(x => x.Usuarios).ToListAsync();
+            return await _dbContext.Tarefas.ToListAsync();
         }
-    }
-
-}
+    }    
+}      
