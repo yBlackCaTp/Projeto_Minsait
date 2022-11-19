4cff300eef1355c332c80c8bd1986a6a0c3f907f
Paulo Murilo <72311432+yBlackCaTp@users.noreply.github.com>
2022-11-19 01:19:02 -0300

    Add files via upload
    V5

--- SistemaDeTarefas/Migrations/20221119035856_initial.Designer.cs
+++ SistemaDeTarefas/Migrations/20221119035856_initial.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using SistemaDeTarefas.Data;
@@ -11,9 +12,11 @@ using SistemaDeTarefas.Data;
 namespace SistemaDeTarefas.Migrations
 {
     [DbContext(typeof(SistemaDeTarefasDBContext))]
-    partial class SistemaDeTarefasDBContextModelSnapshot : ModelSnapshot
+    [Migration("20221119035856_initial")]
+    partial class initial
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
